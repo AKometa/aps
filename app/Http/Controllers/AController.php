2d11dd87d4613072a55dd3a2319b5daf1208b1cd
AKometa <55772775+AKometa@users.noreply.github.com>
2019-10-15 15:36:15 -0300

    Update AController.php

--- app/Http/Controllers/AController.php
+++ app/Http/Controllers/AController.php
@@ -10,5 +10,9 @@ class AController extends Controller
     public function enviaContato(Request $request)
     {
       Mail::to('apsdewebalexandre@gmail.com')->send(new ContatoEmail($request));
+      $name = $request->input('nome');
+      $email = $request->input('email');
+      $msg = $request->input('mensagem');
+      echo "<h1>Foi enviado para nosso E-mail de contato: </h1> Nome:",$name,"<br>E-mail:",$email,"<br>Mensagem:",$msg;
     }
 }
